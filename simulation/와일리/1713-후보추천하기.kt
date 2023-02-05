c0b5f6a192a5a93abe78e103219ea4e0f1156e4a
Sangwoo Nam (Waily) <nsw5296@naver.com>
2023-02-05 15:48:40 +0900

    Update 1713-후보추천하기.kt

--- simulation/와일리/1713-후보추천하기.kt
+++ simulation/와일리/1713-후보추천하기.kt
@@ -1,9 +1,3 @@
-import java.io.BufferedReader
-import java.io.BufferedWriter
-import java.io.InputStreamReader
-import java.io.OutputStreamWriter
-import java.util.*
-
 fun main() {
     val br = BufferedReader(InputStreamReader(System.`in`))
     val bw = BufferedWriter(OutputStreamWriter(System.`out`))
